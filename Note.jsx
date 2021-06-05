fabc04b205ae01db9ecac1a1f53c46e3865be5da
Vaishak Maharaj <68328397+VaishakM2001@users.noreply.github.com>
2021-06-05 16:10:19 +0100

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>My Goal!</h1>
+      <p>My goal is to become a professional Web Developer and create awesome websites for famous companies </p>
+    </div>
+  );
+}
+
+export default Note;
